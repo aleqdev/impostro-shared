5cc215a05aa486a23d847ee06926af083b21e90e
necromfox <necromfox@gmail.com>
2022-05-09 16:29:11 +0300

    add Session::new

--- src/session/mod.rs
+++ src/session/mod.rs
@@ -17,6 +17,13 @@ pub struct Session {
 }
 
 impl Session {
+    pub fn new(counter: &mut SessionCounter) -> Self {
+        Self {
+            id: SessionId::new_random(counter),
+            groups: SessionGroups::default()
+        }
+    }
+
     pub fn get_group<'a, 'b: 'a>(&'b self, name: GroupName) -> Option<&'a Group> {
         for g in self.groups.iter() {
             if g.name == name {
